1821cc9c917835d363683413835ae425858189c1
Frank Morton <fmorton@mac.com>
2021-11-08 09:53:39 -0500

    prototype soap_box_derby_1

--- fmorton/birdbrain_python/soap_box_derby_1/soap_box_derby_1.py
+++ fmorton/birdbrain_python/soap_box_derby_1/soap_box_derby_1.py
@@ -1,12 +1,57 @@
 from BirdBrain import Hummingbird
 import time
 
-print("BirdBrain")
+DISTANCE_TARGET = 75
+DISTANCE_SAFE_ZONE_WIDTH = 16
+DISTANCE_SAMPLE_MAX = 200.0
+DISTANCE_SAMPLE_COUNT = 20         # bigger number means smaller adjustments
+DISTANCE_ADJUSTMENT_DIVISOR = 7.0  # bigger number means smaller adjustments
+STEER_SERVO_NUMBER = 1
+STEER_STRAIGHT = 90
+STEER_FREQUENCY_SLEEP = 0.1
 
 robot = Hummingbird('A')
 
-while True:
-    distance = robot.getDistance(1)
-    print(distance)
+robot.setPositionServo(STEER_SERVO_NUMBER, STEER_STRAIGHT)
 
-    time.sleep(0.1)
+distance_queue = []
+
+while True:
+    #----------------------------------------------------------------------------------------------
+    #  get current distance from railing and ignore if unreasonable
+    #----------------------------------------------------------------------------------------------
+    distance_sample = robot.getDistance(1)
+
+    if distance_sample > DISTANCE_SAMPLE_MAX: continue
+
+    #----------------------------------------------------------------------------------------------
+    #  get a moving average of the distance from the railing
+    #----------------------------------------------------------------------------------------------
+    if len(distance_queue) >= DISTANCE_SAMPLE_COUNT: distance_queue.pop(0)
+
+    distance_queue.append(distance_sample)
+
+    distance = sum(distance_queue) / len(distance_queue)
+
+    distance_off_of_center = DISTANCE_TARGET - distance
+
+    #----------------------------------------------------------------------------------------------
+    #  go straight if in the center of the distance window
+    #----------------------------------------------------------------------------------------------
+    if abs(distance_off_of_center) <= DISTANCE_SAFE_ZONE_WIDTH:
+        print('straight')
+        robot.setPositionServo(STEER_SERVO_NUMBER, STEER_STRAIGHT)
+        continue
+
+    #----------------------------------------------------------------------------------------------
+    #  adjust steering
+    #----------------------------------------------------------------------------------------------
+    distance_adjustment = distance_off_of_center / DISTANCE_ADJUSTMENT_DIVISOR
+
+    steering_adjustment = STEER_STRAIGHT - distance_adjustment
+
+    print(distance_queue, '-->', distance, ' off=', distance_off_of_center," adjust=",distance_adjustment, '  new angle is.............. ',steering_adjustment)
+
+    robot.setPositionServo(STEER_SERVO_NUMBER, steering_adjustment)
+
+    time.sleep(STEER_FREQUENCY_SLEEP)
