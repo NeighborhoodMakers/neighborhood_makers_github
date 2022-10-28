16862e21a31711d799127246b3af344e7ca84393
Frank Morton <fmorton@mac.com>
2022-10-28 10:58:05 -0400

    make hummingbird-robot devices optional

--- fmorton/hummingbird_python/hummingbird_robot_test/HummingbirdRobot.py
+++ fmorton/hummingbird_python/hummingbird_robot_test/HummingbirdRobot.py
@@ -14,27 +14,39 @@ class HummingbirdRobot:
         self.joystick_calculator = joystick_calculator
         self.joystick_rotation = joystick_rotation
 
-        if self.minimum_motor_speed is None: self.minimum_motor_speed = HummingbirdDualMotorDriver.MINIMUM_SPEED
-        if self.joystick_rotation is None: self.joystick_rotation = HummingbirdJoystick.DEFAULT_ROTATION
-
         self.motors = None
         self.joystick = None
 
+        self.init_motors(motor_device, minimum_motor_speed)
+        self.init_joystick(joystick_device, joystick_calculator, joystick_rotation)
+
+    def init_motors(self, motor_device = 'A', minimum_motor_speed = None):
+        if motor_device is not None: self.motor_device = motor_device
+
         if self.motor_device is not None:
-            try:
-                self.motors = HummingbirdDualMotorDriver(self.motor_device, self.minimum_motor_speed)
-            except ConnectionRefusedError:
-                print("Motor device not available")
-                raise
+            if self.minimum_motor_speed is None: self.minimum_motor_speed = HummingbirdDualMotorDriver.MINIMUM_SPEED
+
+            if self.motor_device is not None:
+                try:
+                    self.motors = HummingbirdDualMotorDriver(self.motor_device, self.minimum_motor_speed)
+                except ConnectionRefusedError:
+                    print("Motor device not available")
+                    raise
+
+    def init_joystick(self, joystick_device = None, joystick_calculator = None, joystick_rotation = None):
+        if joystick_device is not None: self.joystick_device = joystick_device
 
         if self.joystick_device is not None:
-            try:
-                self.joystick = None if self.joystick_device is None else HummingbirdJoystick(self.joystick_device, self.joystick_rotation)
+            if self.joystick_rotation is None: self.joystick_rotation = HummingbirdJoystick.DEFAULT_ROTATION
 
-                if self.joystick_calculator is None: self.joystick_calculator = HummingbirdJoystickCalculator()
-            except ConnectionRefusedError:
-                print("Joystick device not available")
-                raise
+            if self.joystick_device is not None:
+                try:
+                    self.joystick = None if self.joystick_device is None else HummingbirdJoystick(self.joystick_device, self.joystick_rotation)
+
+                    if self.joystick_calculator is None: self.joystick_calculator = HummingbirdJoystickCalculator()
+                except ConnectionRefusedError:
+                    print("Joystick device not available")
+                    raise
 
     def move(self):
         if self.motors is not None:
--- fmorton/hummingbird_python/hummingbird_robot_test/joystick_robot.py
+++ fmorton/hummingbird_python/hummingbird_robot_test/joystick_robot.py
@@ -4,7 +4,8 @@ from DoesNotMoveJoystickCalculator import *
 
 #robot = HummingbirdRobot('A', 'B', joystick_calculator=DoesNotMoveJoystickCalculator())
 
-robot = HummingbirdRobot('A', 'B', joystick_rotation = 0)
+#robot = HummingbirdRobot('A', 'B', joystick_rotation = 0)
+robot = HummingbirdRobot(None)
 
 while True:
     robot.move()
