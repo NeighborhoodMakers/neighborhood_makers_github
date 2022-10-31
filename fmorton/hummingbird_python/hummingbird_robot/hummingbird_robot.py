60c1927be6879de682ebb9c2e391bcc4d0d6b943
Frank Morton <fmorton@mac.com>
2022-10-31 14:17:38 -0400

    add threading to the hummingbird_robot

--- fmorton/hummingbird_python/hummingbird_robot/hummingbird_robot.py
+++ fmorton/hummingbird_python/hummingbird_robot/hummingbird_robot.py
@@ -1,16 +1,28 @@
+import threading
 import time
 from HummingbirdRobot import *
 
+def move_robot(robot):
+    while True:
+        robot.move()
+
+def red_button_down(robot):
+    print("RED BUTTON DOWN")
+
+def green_button_down(robot):
+    print("GREEN BUTTON DOWN")
+
 robot = HummingbirdRobot('A', 'B', 'C', joystick_rotation = 0)
 
-while True:
-    robot.move()
+joystick_thread = threading.Thread(target=move_robot, args=(robot,))
+joystick_thread.start()
 
+while True:
     if robot.button_down(1):
-        print("RED BUTTON DOWN")
-        time.sleep(3)
+        threading.Thread(target=red_button_down, args=(robot,)).start()
 
     if robot.button_down(2):
-        print("GREEN BUTTON DOWN")
+        threading.Thread(target=green_button_down, args=(robot,)).start()
 
     time.sleep(0.05)
+
